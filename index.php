5db1dc125c556a25cc3ea119ce6f3af45ece3770
Naveed Jamali <naveedjamali@outlook.com>
2021-10-27 19:34:43 +0500

    changed number to decimal in percentage field.

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     <div class="form">
         <form action="grade.php" method="GET">
             <label class="label" for="percentage">Enter your percentage below</label>
-            <input type="number" name="percentage" id="percentage" class="input field" placeholder="Your percentage" required/>
+            <input type="decimal" name="percentage" id="percentage" class="input field" placeholder="Your percentage" required/>
             <input type="submit" class="button" value="Check Grade">
         </form>
     </div>
